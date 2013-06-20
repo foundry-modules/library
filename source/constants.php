63186b0281ca27cd32c838fcbac880a7be6051dc
Jensen Tonne <jensen.tonne@gmail.com>
2013-06-20 16:28:42 +0800

    Added constants.

---
+++ source/constants.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package		Foundry
+ * @copyright	Copyright (C) 2010 - 2013 Stack Ideas Sdn Bhd. All rights reserved.
+ * @license		GNU/GPL, see LICENSE.php
+ *
+ * Foundry is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ * See COPYRIGHT.php for copyright notices and details.
+ */
+
+defined('_JEXEC') or die('Restricted access');
+
+define('FOUNDRY_VERSION' , $$FOUNDRY_VERSION);
+define('FOUNDRY_PATH'    , JPATH_ROOT . DIRECTORY_SEPARATOR . 'media' . DIRECTORY_SEPARATOR . 'foundry' . DIRECTORY_SEPARATOR . FOUNDRY_VERSION);
+define('FOUNDRY_CLASSES' , FOUNDRY_PATH . DIRECTORY_SEPARATOR . 'joomla');
+define('FOUNDRY_LIB'     , FOUNDRY_PATH . DIRECTORY_SEPARATOR . 'libraries');
