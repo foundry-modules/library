e948606d5c4796312f89519004bb446b7bbb1c60
Jensen Tonne <jensen.tonne@gmail.com>
2013-06-25 18:47:09 +0800

    Fixed wrong dispatch format.

--- source/compiler/static.php
+++ source/compiler/static.php
@@ -42,7 +42,7 @@ if ($compileMode=='static') {
 	// 1. Foundry (foundry.js)
 	echo $this->getFoundry();
 
-	echo 'dispatch.to("Foundry/' . FOUNDRY_VERSION . '").at(function($){' . "\n";
+	echo 'dispatch.to("Foundry/' . FOUNDRY_VERSION . ' Core Plugins").at(function($, manifest) {' . "\n";
 
 	if (!empty($deps['Foundry'])) {
 
