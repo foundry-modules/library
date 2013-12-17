7deabaea76ce38abc4cd092dd73018ae0d6ad8ae
Jensen Tonne <jstonne@stackideas.com>
2013-12-17 11:10:33 +0800

    Added task name and start/stop/resolve/reject logging.

--- source/stylesheet/task.php
+++ source/stylesheet/task.php
@@ -12,6 +12,7 @@ class %BOOTCODE%_Stylesheet_Task {
 	const STATE_PENDING = 'pending';
 
 	// Compile summary
+	public $name;
 	public $state;
 	public $message = '';
 	public $details = array();
@@ -26,44 +27,68 @@ class %BOOTCODE%_Stylesheet_Task {
 	public $mem_end;
 	public $mem_peak;
 
-	public function __construct() {
+	public function __construct($name='Task', $autostart=true) {
+
 		$this->state = self::STATE_PENDING;
-		$this->start();
+		$this->name = $name;
+
+		if ($autostart) $this->start();
 	}
 
-	private function start() {
+	private function start($message='', $type=self::MESSAGE_INFO) {
+
+		if (empty($message)) $message = "$this->name started.";
+
 		$this->time_start = microtime(true);
 		$this->mem_start = memory_get_usage();
+		$this->report($message, $type);
+
+		return $this;
 	}
 
-	private function stop() {
+	private function stop($message='', $type=self::MESSAGE_INFO) {
+
+		if (empty($message)) $message = "$this->name stopped.";
+
 		$this->time_end = microtime(true);
 		$this->time_total = $this->time_end - $this->time_start;
 		$this->mem_end = memory_get_usage();
 		$this->mem_peak = memory_get_peak_usage();
-	}
+		$this->report($message);
 
-	public function resolve($message='') {
-		$this->message = empty($message) ? 'Compile successful!' : JText::_($message);
-		$this->state = self::STATE_SUCCESS;
-		$this->failed = false;
-		$this->stop();
 		return $this;
 	}
 
-	public function reject($message='') {
-		$this->message = empty($message) ? 'Unknown error occured!' : JText::_($message);
+	public function resolve($message='', $type=self::MESSAGE_SUCCESS) {
+
+		if (empty($message)) $message = "$this->name completed.";
+
+		$this->message = $message;
+		$this->state = self::STATE_SUCCESS;
+		$this->failed = false;
+		$this->stop($message, $type);
+
+		return $this;
+	}
+
+	public function reject($message='', $type=self::MESSAGE_ERROR) {
+
+		if (empty($message)) $message = "$this->name failed.";
+
+		$this->message = $message;
 		$this->state = $self::STATE_ERROR;
 		$this->failed = true;
-		$this->stop();
+		$this->stop($message, $type);
+
 		return $this;
 	}
 
 	public function report($message='', $type=self::MESSAGE_WARN) {
 
 		$detail = (object) array(
-			'message' => $message;
-			'type'    => $type;
+			'timestamp' => time(),
+			'message'   => $message,
+			'type'      => $type
 		);
 
 		$details[] = $detail;
