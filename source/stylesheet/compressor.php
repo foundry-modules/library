be07246d0d4de019d7e03e00f6eb64e23c122d87
Jensen Tonne <jstonne@stackideas.com>
2013-12-20 12:12:24 +0800

    Added stylesheet compressor.

--- source/stylesheet/compressor.php
+++ source/stylesheet/compressor.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once(%BOOTCODE%_FOUNDRY_LIB . '/cssmin.php');
+require_once(%BOOTCODE%_FOUNDRY_CLASSES . '/stylesheet/task.php');
 
 class %BOOTCODE%_Stylesheet_Compressor extends %BOOTCODE%_Libraries_CSSMin {
 
@@ -9,5 +10,54 @@ class %BOOTCODE%_Stylesheet_Compressor extends %BOOTCODE%_Libraries_CSSMin {
 	public function __construct($stylesheet) {
 
 		$this->stylesheet = $stylesheet;
+	}
+
+	public function run($section) {
+
+		// Create new task
+		$this->task = %BOOTCODE%_Stylesheet_Task("Minify section '$section'");
+		$task = $this->task;
+
+		// Get paths
+		$in   = $this->stylesheet->file($section, 'css');
+		$out  = $this->stylesheet->file($section, 'minified');
+		$root = dirname($out);
+
+		// Check if css file exists.
+		if (!JFile::exists($in)) {
+			return $task->reject("Missing css file '$in'.");
+		}
+
+		// Check if folder is writable.
+		if (!is_writable($root)) {
+			return $task->reject("Unable to write files inside the folder '$root'.");
+		}
+
+		// Check if css file is writable.
+		if (JFile::exists($out) && !is_writable($out)) {
+			return $task->reject("Unable to write css file '$out'.");
+		}
+
+		$content = JFile::read($in);
+
+		if ($content===false) {
+			return $task->reject("Unable to read css file '$in'.");
+		}
+
+		$minifiedContent = null;
+
+		try {
+			$minifiedContent = parent::run($content);
+		} catch (Exception $exception) {
+			$task->reject("An error occured while minifying section '$section'.");
+			$task->report($exception->getMessage(), 'error');
+			return $task;
+		}
+
+		if (!JFile::write($out, $minifiedContent)) {
+			return $task->reject("An error occured while writing minified file '$out'.");
+		}
+
+		return $task->resolve();
 	}
 }
